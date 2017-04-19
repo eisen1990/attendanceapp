5fb03a7fba048035b16408590bf7ad351bd2da64
eisen <eisen1990@naver.com>
2017-04-19 18:28:22 +0900

    Wi-Fi 탐색 테스트 및 UI 업데이트 추가
    특정 SSID의 MAC을 얻어오도록 설계, 현재 MAC 주소 얻어올 경우 최하위 값이 하나가 빠진다.
    UI에서 시계 업데이트 추가

--- app/src/main/java/com/planetory/io/MainActivity.java
+++ app/src/main/java/com/planetory/io/MainActivity.java
@@ -1,7 +1,16 @@
 package com.planetory.io;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -14,10 +23,101 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
+
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    TextView dateNow;
+    TextView wifiScan;
+
+    Handler handler = new Handler(new Handler.Callback() {
+
+        @Override
+        public boolean handleMessage(Message msg) {
+            if(msg.what == 0) {
+                String time = (String) msg.obj;
+                dateNow.setText(time);
+            }
+            return true;
+        }
+    });
+
+    public String getFormatDateTime() {
+        /*
+            현재 시간을 문자열로 반환하는 함수.
+            Main Activity에서 시계 UI를 업데이트하는 Thread에서 사용된다.
+         */
+        long now = System.currentTimeMillis();
+        Date date = new Date(now);
+
+        return new SimpleDateFormat("HH:mm:ss").format(date);
+    }
+
+    public String scanWifi() {
+        /*
+            출근 버튼을 누를 경우 수행되는 Wi-Fi 스캔 함수.
+         */
+
+        //Wifi 관련 클래스 정의
+        WifiManager wifiManager;
+        ConnectivityManager connManager;
+        List<ScanResult> scanResult;
+        SimpleAdapter adapter;
+        ArrayList<HashMap<String,String>> list = new ArrayList<HashMap<String, String>>();
+
+        String MACAddr = null;
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+//        connManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+//        NetworkInfo wifiInfo = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+
+        scanResult = wifiManager.getScanResults();
+        list.clear();
+
+        for(ScanResult result: scanResult) {
+            HashMap<String,String> item = new HashMap<String,String>();
+            item.put("bssid","BSSID : " + result.BSSID);
+            item.put("ssid","SSID : " + result.SSID);
+            item.put("capabilities","Capablities : " + result.capabilities);
+            item.put("freq","Freq : " + result.frequency);
+            item.put("level","Signal Level : " + result.level);
+            list.add(item);
+            if(result.SSID.equals("EASWLabs")) MACAddr = result.BSSID;
+        }
+        /*
+        String[] from = new String[] {
+                "bssid",
+                "ssid",
+                "capabilities",
+                "freq",
+                "level"
+        };
+
+        int[] to = new int[] {
+                R.id.bssid,
+                R.id.ssid,
+                R.id.capabilities,
+                R.id.Freq,
+                R.id.level
+        };
+
+        adapter = new SimpleAdapter( this, list, R.layout.activity_main, from, to);
+
+//        setListAdapter(adapter);
+        */
+
+        return MACAddr;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +126,34 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        dateNow = (TextView) findViewById(R.id.dateNow);
+        wifiScan = (TextView) findViewById(R.id.wifi);
+
+        Thread timerThread = new Thread(new Runnable() {
+            /*
+                Main Activity의 UI 업데이트를 담당하는 Thread의 Runnable 구현
+                초기에 thread.run()으로 할 때 메인 Thread의 콜스택을 사용해서 안됐었음.
+                thread.start()로 실행하니 정상작동함.
+             */
+            @Override
+            public void run() {
+                while(true) {
+                    try {
+                        Message msg = handler.obtainMessage();
+                        msg.what = 0;
+                        msg.obj = getFormatDateTime();
+                        handler.sendMessage(msg);
+
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) { e.printStackTrace(); }
+                }
+            }
+        });
+        timerThread.start();
+
+        /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,20 +161,15 @@ public class MainActivity extends AppCompatActivity
                         .setAction("Action", null).show();
             }
         });
-
-
-        /*
-            근무 시작 버튼, Service Thread 시작으로 간다.
-            startService(intent);
         */
+
         Button StartBtn = (Button)  findViewById(R.id.start_btn);
         StartBtn.setOnClickListener(new View.OnClickListener() {
-            Intent intent;
+
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"Run", Toast.LENGTH_SHORT).show();
-                intent = new Intent(MainActivity.this, TimerService.class);
-                startService(intent);
+                String str = scanWifi();
+                wifiScan.setText(str);
             }
         });
 
--- app/src/main/java/com/planetory/io/TimerService.java
+++ app/src/main/java/com/planetory/io/TimerService.java
@@ -1,11 +1,15 @@
 package com.planetory.io;
 
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
 import android.support.annotation.IntDef;
 
 public class TimerService extends Service {
+
     public TimerService() {
     }
 
@@ -17,11 +21,19 @@ public class TimerService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        return super.onStartCommand(intent, flags, startId);
+        /*
+            Wi-Fi 상태 수행
+         */
+//        super.onStartCommand(intent, flags, startId);
+//        TimerServiceHandler handler = new TimerServiceHandler();
+//        thread = new TimerThread(handler);
+//        thread.start();
+        return START_STICKY;
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
     }
+
 }
