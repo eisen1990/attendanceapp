5111eb5cc9a0c82ce319a57886f9c3dbdad71ff4
Riemann_rbrb <yubi0609@gmail.com>
2017-05-12 19:19:44 +0900

    05.12 중간작업 출근 전 화면 마무리.

--- app/src/main/java/com/planetory/io/MainNewActivity.java
+++ app/src/main/java/com/planetory/io/MainNewActivity.java
@@ -1,26 +1,105 @@
 package com.planetory.io;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
+import android.os.Handler;
+import android.os.Message;
+import android.text.format.DateFormat;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainNewActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private final int LEAVE_TIMER_ID = 0;
+
+    String TIME_AM;
+    String TIME_PM;
+
+    TextView txtLeaveTime;
+    TextView txtLeaveDate;
+    TextView txtLeaveMeridiem;
+
+    Timer leaveTimer;
+
+    Handler handler = new Handler(new Handler.Callback() {
+
+        @Override
+        public boolean handleMessage(Message msg) {
+            if (msg.what == LEAVE_TIMER_ID) {
+                Calendar calendar = Calendar.getInstance();
+
+                String sTime = DateFormat.format("hh:mm:ss", calendar).toString();
+                String sMeridiem = calendar.get(Calendar.HOUR_OF_DAY) >= 12 ? TIME_PM : TIME_AM;
+                CharSequence cDate = calendar.get(Calendar.MONTH) >= 10 ? DateFormat.format("MM월 dd일", calendar) :
+                        DateFormat.format("M월 dd일", calendar);
+                String sDate = cDate.toString() + " (" + toDayofWeek(calendar.get(Calendar.DAY_OF_WEEK)) + ')';
+
+                txtLeaveTime.setText(sTime);
+                txtLeaveMeridiem.setText(sMeridiem);
+                txtLeaveDate.setText(sDate);
+            }
+            return true;
+        }
+    });
+
+
+    public String toDayofWeek(int i){
+        String day = "";
+
+        switch (i) {
+            case 1:
+                day = "일";
+                break;
+            case 2:
+                day = "월";
+                break;
+            case 3:
+                day = "화";
+                break;
+            case 4:
+                day = "수";
+                break;
+            case 5:
+                day = "목";
+                break;
+            case 6:
+                day = "금";
+                break;
+            case 7:
+                day = "토";
+                break;
+        }
+        return day;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_new);
 
+        TIME_AM = getString(R.string.activity_main_leave_am);
+        TIME_PM = getString(R.string.activity_main_leave_pm);
+
+        txtLeaveDate = (TextView) findViewById(R.id.activity_main_leave_txt_date);
+        txtLeaveTime = (TextView) findViewById(R.id.activity_main_leave_txt_time);
+        txtLeaveMeridiem = (TextView) findViewById(R.id.activity_main_leave_txt_meridiem);
+
+        basicSetting();
+        timerSetting();
+    }
+
+    private void basicSetting(){
         //자동 생성된 기본 설정
         Toolbar toolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
         setSupportActionBar(toolbar);
@@ -33,9 +112,20 @@ public class MainNewActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.activity_main_nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
 
-
-
+    private void timerSetting(){
+        leaveTimer = new Timer(true);
+        leaveTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                try{
+                    handler.sendEmptyMessage(LEAVE_TIMER_ID);
+                } catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }, 0, 1000);
     }
 
 
@@ -72,5 +162,11 @@ public class MainNewActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    protected void onDestroy() {
+        leaveTimer.cancel();
+        super.onDestroy();
     }
 }
